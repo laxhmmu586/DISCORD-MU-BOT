33adc52f449060dfb8de422f08382403ee304c23
laxhmmu586 <laxhmmu@gmail.com>
2026-08-22 17:00:29 -0700

    Use provided authorization form Drive file

--- test/cbsWorldTracerEmail.test.js
+++ test/cbsWorldTracerEmail.test.js
@@ -6,7 +6,7 @@ const path = require('node:path');
 const server = fs.readFileSync(path.join(__dirname, '..', 'index.js'), 'utf8');
 
 test('WorldTracer update email does not regenerate or attach a PDF report', () => {
-  const updateEmailBlock = server.match(/if \(updateFields\.updateEvent\?\.key === 'worldtracer'\) \{([\s\S]*?)\n\s*\}\n\s*return res\.json/)?.[1] || '';
+  const updateEmailBlock = server.match(/if \(updateFields\.updateEvent\?\.key === 'worldtracer'\) \{([\s\S]*?)\n\s*if \(updateFields\.updateEvent\?\.key === 'requested_bags'\)/)?.[1] || '';
   assert.match(updateEmailBlock, /sendCbsCaseEmail/);
   assert.match(updateEmailBlock, /ccOperations: false/);
   assert.doesNotMatch(updateEmailBlock, /createPirPdf|pdfBuffer|filename/);
